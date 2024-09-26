08863a65c9f845fd4ca71eaf4baad4a786869be5
Rajeev Menon <rajeev.menon@arista.com>
2024-09-26 04:18:41 -0700

    Add treesitter plugin

---
+++ nvim/dot-config/nvim/lua/rajeev/plugins/treesitter.lua
@@ -0,0 +1,77 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPre", "BufNewFile" },
+    build = ":TSUpdate",
+    dependencies = {
+        "windwp/nvim-ts-autotag",
+    },
+    config = function()
+        -- import nvim-treesitter plugin
+        local treesitter = require("nvim-treesitter.configs")
+
+        -- configure treesitter
+        treesitter.setup({
+            -- enable syntax highlighting
+            highlight = {
+                enable = true,
+            },
+            -- enable indentation
+            indent = { enable = true },
+            -- enable autotagging (w/ nvim-ts-autotag plugin)
+            autotag = {
+                enable = true,
+            },
+            -- ensure these language parsers are installed
+            ensure_installed = {
+                "bash",
+                "c",
+                "cmake",
+                "cpp",
+                "csv",
+                "diff",
+                "dockerfile",
+                "git_config",
+                "git_rebase",
+                "gitattributes",
+                "gitcommit",
+                "gitignore",
+                "go",
+                "gpg",
+                "html",
+                "http",
+                "java",
+                "javascript",
+                "json",
+                "llvm",
+                "lua",
+                "make",
+                "ninja",
+                "objdump",
+                "passwd",
+                "perl",
+                "php",
+                "powershell",
+                "proto",
+                "python",
+                "regex",
+                "rust",
+                "sql",
+                "ssh_config",
+                "tmux",
+                "vim",
+                "xml",
+                "yaml",
+                "yang",
+            },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<C-space>",
+                    node_incremental = "<C-space>",
+                    scope_incremental = false,
+                    node_decremental = "<bs>",
+                },
+            },
+        })
+    end,
+}
