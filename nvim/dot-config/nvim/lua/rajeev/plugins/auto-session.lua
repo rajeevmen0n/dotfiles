b84780d7c15805818aeb4267611a4214b714db5d
Rajeev Menon <rajeev.menon@arista.com>
2024-10-01 20:20:08 +0000

    Use correct options for auto-session plugin

--- nvim/dot-config/nvim/lua/rajeev/plugins/auto-session.lua
+++ nvim/dot-config/nvim/lua/rajeev/plugins/auto-session.lua
@@ -3,12 +3,11 @@ return {
     config = function()
         local auto_session = require("auto-session")
 
-        auto_session.setup(
-            {
-                auto_restore_enabled = false,
-                auto_session_suppress_dirs = {"~/", "~/Downloads", "~/Documents", "~/Desktop/", "~/tmp", "/tmp"}
-            }
-        )
+        auto_session.setup({
+            auto_save = true,
+            auto_restore = false,
+            suppressed_dirs = {"~/", "~/Downloads", "~/Documents", "~/Desktop/", "~/tmp", "/tmp"},
+        })
 
         local keymap = vim.keymap
 
