3d6354a91d7e72383b87862f4c30f51babca4c92
Rajeev Menon <rajeev.menon@arista.com>
2024-11-12 12:07:23 -0800

    Wezterm config updates

--- wezterm/dot-wezterm.lua
+++ wezterm/dot-wezterm.lua
@@ -5,12 +5,12 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.font = wezterm.font("MesloLGS Nerd Font Mono")
-config.font_size = 13
+config.font_size = 12
 
-config.enable_tab_bar = false
+config.enable_tab_bar = true
 config.window_decorations = "RESIZE"
 
-config.window_background_opacity = 0.7
+config.window_background_opacity = 0.8
 config.macos_window_background_blur = 50
 
 --colorscheme
