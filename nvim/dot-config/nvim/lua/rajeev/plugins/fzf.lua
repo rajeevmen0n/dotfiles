31dc8be3ddfe64358d19a4cd9d778c75c85ff76f
Rajeev Menon <rajeevrmenon97@gmail.com>
2024-11-23 04:11:59 -0800

    Modify keymaps, remove mini.clue

--- nvim/dot-config/nvim/lua/rajeev/plugins/fzf.lua
+++ nvim/dot-config/nvim/lua/rajeev/plugins/fzf.lua
@@ -9,5 +9,7 @@ return {
         vim.keymap.set("n", "<leader>fr", fzf.oldfiles, { desc = "Fzf Recent files" })
         vim.keymap.set("n", "<leader>fc", fzf.grep_curbuf, { desc = "Fzf find in file" })
         vim.keymap.set("n", "<leader>fs", fzf.live_grep, { desc = "Fzf live grep" })
+        vim.keymap.set("n", "<leader>fb", fzf.buffers, { desc = "Fzf buffers" })
+        vim.keymap.set("n", "<leader>fk", fzf.keymaps, { desc = "Fzf keymaps" })
     end
 }
