d0a61be114dd727659ec54c247c1ec91e7e5a81f
Rajeev Menon <rajeev.menon@arista.com>
2025-07-10 19:05:22 +0000

    Fix mason after update

--- nvim/dot-config/nvim/lua/rajeev/plugins/lsp/lspconfig.lua
+++ nvim/dot-config/nvim/lua/rajeev/plugins/lsp/lspconfig.lua
@@ -64,13 +64,15 @@ return {
         local cmp_nvim_lsp = require("cmp_nvim_lsp")
         local capabilities = cmp_nvim_lsp.default_capabilities()
 
-        mason_lspconfig.setup_handlers({
-            -- default handler for installed servers
-            function(server_name)
-                lspconfig[server_name].setup({
-                    capabilities = capabilities,
-                })
-            end,
+        mason_lspconfig.setup({
+            handlers = {
+                -- default handler for installed servers
+                function(server_name)
+                    lspconfig[server_name].setup({
+                        capabilities = capabilities,
+                    })
+                end,
+            },
         })
     end,
 }
