2376cb1924e5c35e9e968e7640f5eaeb49e29da6
Rajeev Menon <rajeevrmenon@proton.me>
2025-11-06 05:11:29 -0800

    Update deprecated syntax for diagnostic symbols

--- nvim/dot-config/nvim/lua/rajeev/plugins/lsp/lspconfig.lua
+++ nvim/dot-config/nvim/lua/rajeev/plugins/lsp/lspconfig.lua
@@ -53,11 +53,16 @@ return {
         })
 
         -- Change the Diagnostic symbols in the sign column (gutter)
-        local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-        for type, icon in pairs(signs) do
-            local hl = "DiagnosticSign" .. type
-            vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-        end
+        vim.diagnostic.config({
+            signs = {
+                text = {
+                    [vim.diagnostic.severity.ERROR] = " ",
+                    [vim.diagnostic.severity.WARN] = " ",
+                    [vim.diagnostic.severity.HINT] = "󰠠 ",
+                    [vim.diagnostic.severity.INFO] = " ",
+                },
+            },
+        })
 
         local mason_lspconfig = require("mason-lspconfig")
         local cmp_nvim_lsp = require("cmp_nvim_lsp")
